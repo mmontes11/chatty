e15c85f637b54f1c60896b7761e23b6333d1c5f2
Martín Montes Pérez <martin.montes@nomasystems.com>
2019-08-17 17:50:17 +0200

    Changed collection name for dev

--- src/config/development.js
+++ src/config/development.js
@@ -1,7 +1,7 @@
 export default {
   env: "development",
   nodePort: 8080,
-  mongoUrl: "mongodb://localhost:27017/chatty-server",
+  mongoUrl: "mongodb://localhost:27017/chatty",
   jwtSecret: "oxJWWSjUKC",
   debug: true,
 };

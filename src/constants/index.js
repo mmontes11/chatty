dec6c725e9b0042a22819f6ecaadd8bdd90682f2
Martín Montes Pérez <martin.montes@nomasystems.com>
2019-08-23 20:32:23 +0200

    Returning not found error in pagination

--- src/constants/index.js
+++ src/constants/index.js
@@ -2,3 +2,4 @@ export const TOKEN_EXPIRATION = "30m";
 export const PASSWORD_REGEX = /^(?=.*[A-Z])(?=.*[a-z])(?=.*[!@#$&*])(?=.*[0-9]).{8,}$/;
 export const ROLE_USER = "USER";
 export const ROLE_ADMIN = "ADMIN";
+export const ERROR_NOT_FOUND_CODE = "NOT_FOUND";
--- src/helpers/pagination.js
+++ src/helpers/pagination.js
@@ -1,4 +1,6 @@
+import { ApolloError } from "apollo-server";
 import { encode, decode } from "./base64";
+import { ERROR_NOT_FOUND_CODE } from "../constants";
 
 export const createCursorPaginator = (Entity, sortField) => async (cursor, limit = 10, filter = {}) => {
   const cursorOpts = cursor ? { ...filter, [sortField]: { $lt: decode(cursor) } } : filter;
@@ -9,6 +11,9 @@ export const createCursorPaginator = (Entity, sortField) => async (cursor, limit
   const totalCount = await Entity.countDocuments(filter);
   const hasNextPage = result.length > limit;
   const edges = hasNextPage ? result.slice(0, -1) : result;
+  if (result.length === 0) {
+    throw new ApolloError("Not found", ERROR_NOT_FOUND_CODE);
+  }
   const lastSortField = edges[edges.length - 1][sortField].toString();
   const endCursor = encode(lastSortField);
   return {

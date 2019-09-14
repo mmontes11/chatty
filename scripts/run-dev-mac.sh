639da648c8bc50db02ea107fa761c790596c994b
Martín Montes Pérez <martin.montes@nomasystems.com>
2019-09-14 12:01:56 +0200

    Waiting for mongo availability

--- scripts/run-dev-mac.sh
+++ scripts/run-dev-mac.sh
@@ -4,5 +4,6 @@
 # ttab: https://github.com/mklement0/ttab
 
 docker-compose -f docker-compose.mongo.yml up -d
+echo "Starting... 🚀"
 ttab -t "back" "cd packages/back; npm start"
 ttab -t "front" "cd packages/front; npm start"
